12ccc4ac95ade784f05bab3c23bfa55fb6a215ee
Abdulbaki Dursun <eggsydev@gmail.com>
2023-12-27 01:29:21 +0300

    refactor: seo for blog posts

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -6,6 +6,7 @@ import Image from "next/image";
 import { TbCalendar } from "react-icons/tb";
 import { urlForImage } from "../../sanity/lib/image";
 import type { Image as SanityImage } from "sanity";
+import { NextSeo } from "next-seo";
 
 export interface Post extends SanityDocument {
   title: string;
@@ -29,56 +30,67 @@ export default function BlogPost({ post }: { post: Post }) {
   if (!post) return null;
 
   return (
-    <main className="container-app">
-      {post.mainImage && (
-        <div className="relative w-full overflow-hidden h-[150px] md:h-[350px] rounded-lg mb-6">
-          <Image
-            src={urlForImage(post.mainImage ?? "")}
-            alt={(post.mainImage.alt as string) ?? ""}
-            fill
-            style={{
-              objectFit: "cover",
+    <>
+      <NextSeo
+        title={post.title}
+        titleTemplate="%s - SWY35 Türkiye Blog"
+        openGraph={{
+          title: post.title,
+          images: [{ url: urlForImage(post.mainImage! ?? "") }],
+        }}
+      />
+
+      <main className="container-app">
+        {post.mainImage && (
+          <div className="relative w-full overflow-hidden h-[150px] md:h-[350px] rounded-lg mb-6">
+            <Image
+              src={urlForImage(post.mainImage ?? "")}
+              alt={(post.mainImage.alt as string) ?? ""}
+              fill
+              style={{
+                objectFit: "cover",
+              }}
+            />
+          </div>
+        )}
+
+        <div className="flex mb-4 flex-wrap items-start justify-between">
+          <h1 className="text-4xl font-bold">{post.title}</h1>
+
+          <div className="flex items-center gap-2 text-black/50">
+            <span>
+              {new Date(post.publishedAt).toLocaleDateString("en-US", {
+                year: "numeric",
+                month: "short",
+                day: "numeric",
+                hour: "numeric",
+                minute: "numeric",
+              })}
+            </span>
+
+            <TbCalendar />
+          </div>
+        </div>
+
+        <div className="prose max-w-full prose-blue">
+          <PortableText
+            value={post.body}
+            components={{
+              types: {
+                image: ({ value }) => {
+                  return (
+                    <img
+                      src={urlForImage(value)}
+                      alt={value.alt}
+                      loading="lazy"
+                    />
+                  );
+                },
+              },
             }}
           />
         </div>
-      )}
-
-      <div className="flex mb-4 flex-wrap items-start justify-between">
-        <h1 className="text-4xl font-bold">{post.title}</h1>
-
-        <div className="flex items-center gap-2 text-black/50">
-          <span>
-            {new Date(post.publishedAt).toLocaleDateString("en-US", {
-              year: "numeric",
-              month: "short",
-              day: "numeric",
-              hour: "numeric",
-              minute: "numeric",
-            })}
-          </span>
-
-          <TbCalendar />
-        </div>
-      </div>
-
-      <div className="prose max-w-full prose-blue">
-        <PortableText
-          value={post.body}
-          components={{
-            types: {
-              image: ({ value }) => {
-                return (
-                  <img
-                    src={urlForImage(value)}
-                    alt={value.alt}
-                    loading="lazy"
-                  />
-                );
-              },
-            },
-          }}
-        />
-      </div>
-    </main>
+      </main>
+    </>
   );
 }
