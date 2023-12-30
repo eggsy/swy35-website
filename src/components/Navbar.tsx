42461496afaeac4391bf8a0b1ad746fc03706c15
Abdulbaki Dursun <eggsydev@gmail.com>
2023-12-30 19:55:27 +0300

    refactor: better mobile navbar layout

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useState } from "react";
+import { TbMenu2, TbMenuDeep } from "react-icons/tb";
 
 const pages = [
   {
@@ -27,32 +29,45 @@ const pages = [
 
 export const Navbar = () => {
   const router = useRouter();
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className="flex py-10 items-center justify-between flex-wrap gap-8 md:gap-12 container-app">
-      <Link
-        href="/"
-        className="flex w-full md:w-max flex-col md:flex-row justify-center md:justify-start items-center gap-4"
+    <nav className="flex py-10 items-center justify-between flex-wrap gap-8 lg:gap-12 container-app">
+      <div className="flex items-center justify-between gap-4 w-full lg:w-max">
+        <Link href="/" className="flex items-center gap-4">
+          <Image
+            src="/logo.png"
+            alt="SWY35 Türkiye Delegation logo"
+            width={60}
+            height={60}
+            priority
+          />
+
+          <div className="flex flex-col gap-1 lg:gap-0">
+            <span className="md:text-lg text-xl font-bold text-brand">
+              SWY35 Türkiye
+            </span>
+
+            <span className="text-black/50 hidden lg:block text-xs md:text-sm w-3/4 md:w-full">
+              One ship, many journeys, endless connections!
+            </span>
+          </div>
+        </Link>
+
+        <button
+          type="button"
+          className="block lg:hidden bg-black/5 rounded-lg p-2 hover:bg-black/10 transition-colors"
+          onClick={() => setIsOpen((prev) => !prev)}
+        >
+          {isOpen ? <TbMenuDeep size={26} /> : <TbMenu2 size={26} />}
+        </button>
+      </div>
+
+      <ul
+        className={`
+          ${isOpen ? "flex" : "hidden lg:flex"}
+           flex-col lg:flex-row justify-evenly items-center bg-black/5 md:bg-transparent md:gap-8 rounded-lg overflow-auto w-full md:w-max`}
       >
-        <Image
-          src="/logo.png"
-          alt="SWY35 Türkiye Delegation logo"
-          width={60}
-          height={60}
-          priority
-        />
-
-        <div className="flex flex-col items-center gap-1 lg:gap-0 md:items-start text-center">
-          <span className="md:text-lg text-xl font-bold text-brand">
-            SWY35 Türkiye
-          </span>
-          <span className="text-black/50 text-xs md:text-sm w-3/4 md:w-full">
-            One ship, many journeys, endless connections!
-          </span>
-        </div>
-      </Link>
-
-      <ul className="flex justify-evenly items-center gap-4 md:gap-8 bg-black/5 md:bg-transparent rounded-lg overflow-auto w-full md:w-max">
         {pages.map((page) => (
           <li key={page.path}>
             <Link
@@ -64,6 +79,7 @@ export const Navbar = () => {
                   : "text-gray-500 hover:text-brand"
               }`}
               href={page.path}
+              onClick={() => setIsOpen(false)}
             >
               {page.title}
             </Link>
