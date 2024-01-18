af7c92fbed6e11129e178d97b22abb669edd6efe
Abdulbaki Dursun <eggsydev@gmail.com>
2024-01-18 03:44:04 +0300

    style: fix height mismatch

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -21,7 +21,7 @@ export const Input = ({
       <input
         id={fieldId}
         className={clsx(
-          `peer h-full w-full rounded-[7px] border border-blue-gray-200  bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all  focus:border-gray-900 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50 ${className}`,
+          "peer h-full w-full rounded-[7px] border border-blue-gray-200 bg-transparent px-3 py-[0.700rem] text-sm font-normal text-blue-gray-700 transition-colors focus:border-gray-900 focus:outline-0",
           label && "border-t-transparent focus:border-t-transparent",
           className
         )}
--- src/components/Form/Select.tsx
+++ src/components/Form/Select.tsx
@@ -30,7 +30,7 @@ export const Select = ({
         value={value}
         onChange={onChange}
         className={clsx(
-          `peer h-full appearance-none w-full rounded-[7px] border border-blue-gray-200  bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all focus:border-gray-900 focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50 ${className}`,
+          `peer h-full appearance-none outline outline-0 w-full rounded-[7px] border border-blue-gray-200  bg-transparent px-3 py-2.5 text-sm font-normal text-blue-gray-700 transition-all focus:border-gray-900`,
           label && "border-t-transparent focus:border-t-transparent",
           className
         )}
